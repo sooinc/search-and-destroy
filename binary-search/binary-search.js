00e9e0217f1d7658bb0dd8b77987d8868454f4c4
sooin.c <sooinc90@gmail.com>
2020-02-19 15:45:59 -0500

    complete with binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,34 @@
 'use strict';
 
 // Complete this algo
+
+const split = function(array) {
+  let midPointIndex = Math.floor(array.length / 2);
+
+  let firstHalf = array.slice(0, midPointIndex);
+  let secondHalf = array.slice(midPointIndex);
+
+  return [firstHalf, secondHalf];
+};
+
 const binarySearch = (array, target) => {
-	
+  let splitArr = split(array);
+  let firstHalf = splitArr[0];
+  let secondHalf = splitArr[1];
+  console.log('firsthalf:', firstHalf);
+  console.log('secondhalf:', secondHalf);
+
+  if (target === secondHalf[0] || target === firstHalf[0]) {
+    return true;
+  } else {
+    if (firstHalf.includes(target)) {
+      return binarySearch(firstHalf, target);
+    } else if (secondHalf.includes(target)) {
+      return binarySearch(secondHalf, target);
+    } else {
+      return false;
+    }
+  }
 };
 
 /*
@@ -14,4 +40,12 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
+
+// if (target < secondHalf[0]) {
+// 	return binarySearch(firstHalf, target);
+//   } else if (target > secondHalf[0]) {
+// 	return binarySearch(secondHalf, target);
+//   } else {
+// 	return false;
+//   }
